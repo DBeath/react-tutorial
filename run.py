bc420ebfaa18581d8911dcf0620f4c1c175b1b6c
David Beath <davidgbeath@gmail.com>
2015-01-30 16:40:01 +1300

    Upgraded server

--- run.py
+++ run.py
@@ -1,10 +1,45 @@
-from flask import Flask, Response, render_template, json
+from flask import Flask, Response, render_template, json, request, make_response, jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from datetime import datetime
+
 import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['DEBUG'] = True
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+class Comment(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	author = db.Column(db.String(80))
+	text = db.Column(db.String(1024))
+	posted = db.Column(db.DateTime, default=datetime.utcnow())
+
+	def to_json(self):
+		return {
+			"id": self.id,
+			"author": self.author,
+			"text": self.text,
+			"posted": str(self.posted)
+		}
 
 def root_dir():
 	return os.path.abspath(os.path.dirname(__file__))
+
+def get_comments():
+	comments = Comment.query.all()
+	values = [comment.to_json() for comment in comments]
+	file = open('_comments.json', 'w+')
+	file.write(json.dumps(values))
+	file.close()
+	return values
 
 @app.route('/')
 def index():
@@ -12,13 +47,25 @@ def index():
 
 @app.route('/comments', methods=['GET'])
 def comments():
-	print 'Hit comments url'
-	file = open('_comments.json', 'r+')
-	comments = json.loads(file.read())
-	print comments
-	file.close()
+	response = make_response()
+	response.data = get_comments()
+	print response.data
+	return response
 
-	return json.dumps(comments)
+@app.route('/comments', methods=['POST'])
+def post_comments():
+	print request.form['author']
+	print request.form['text']
+
+	comment = Comment()
+	comment.author = request.form['author']
+	comment.text = request.form['text']
+	db.session.add(comment)
+	db.session.commit()
+
+	response = make_response()
+	response.data = get_comments()
+	return response
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	manager.run()
