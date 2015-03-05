73db479b3a27a32f35a0426867af8249113a4df1
David Beath <davidgbeath@gmail.com>
2015-03-05 17:10:17 +1300

    Added user authentication

--- run.py
+++ run.py
@@ -4,17 +4,38 @@ from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 from datetime import datetime
 from marshmallow import Schema, fields, pprint
+from flask.ext.security import Security, SQLAlchemyUserDatastore, UserMixin, \
+	RoleMixin, login_required
 import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'super-secret'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+roles_users = db.Table('roles_users',
+	db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+	db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+	id = db.Column(db.Integer(), primary_key=True)
+	name = db.Column(db.String(80), unique=True)
+	description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(255), unique=True)
+	password = db.Column(db.String(255))
+	active = db.Column(db.Boolean())
+	confirmed_at = db.Column(db.DateTime())
+	roles = db.relationship('Role', secondary=roles_users,
+							backref=db.backref('users', lazy='dynamic'))
 
 class Comment(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -36,11 +57,16 @@ class CommentSchema(Schema):
 	text = fields.String()
 	posted = fields.DateTime()
 
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
 @app.route('/')
+@login_required
 def index():
 	return render_template('index.html')
 
 @app.route('/comments', methods=['GET', 'POST'])
+@login_required
 def comments():
 	if request.method == 'POST':
 		comment = Comment()
@@ -54,6 +80,11 @@ def comments():
 	result = serializer.dump(comments)
 	response = jsonify({"comments": result.data})
 	return response
+
+@manager.command
+def create_user(email, password):
+	user_datastore.create_user(email=email, password=password)
+	db.session.commit()
 
 if __name__ == '__main__':
 	manager.run()
