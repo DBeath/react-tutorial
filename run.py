bc01ad76404b4d9c4d26daf1854ceded34e101c1
David Beath <davidgbeath@gmail.com>
2016-09-20 21:24:38 +0200

    Updated to latest React version

--- run.py
+++ run.py
@@ -1,17 +1,18 @@
 from flask import Flask, Response, render_template, json, request, make_response, jsonify
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from datetime import datetime
 from marshmallow import Schema, fields, pprint
-from flask.ext.security import Security, SQLAlchemyUserDatastore, UserMixin, \
-	RoleMixin, login_required
-import os
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, \
+    RoleMixin, login_required
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'super-secret'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -20,71 +21,79 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 roles_users = db.Table('roles_users',
-	db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-	db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
 
 class Role(db.Model, RoleMixin):
-	id = db.Column(db.Integer(), primary_key=True)
-	name = db.Column(db.String(80), unique=True)
-	description = db.Column(db.String(255))
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
 
 class User(db.Model, UserMixin):
-	id = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(255), unique=True)
-	password = db.Column(db.String(255))
-	active = db.Column(db.Boolean())
-	confirmed_at = db.Column(db.DateTime())
-	roles = db.relationship('Role', secondary=roles_users,
-							backref=db.backref('users', lazy='dynamic'))
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
 
 class Comment(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	author = db.Column(db.String(80))
-	text = db.Column(db.String(1024))
-	posted = db.Column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(80))
+    text = db.Column(db.String(1024))
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
 
-	def to_json(self):
-		return {
-			"id": self.id,
-			"author": self.author,
-			"text": self.text,
-			"posted": str(self.posted)
-		}
+    def to_json(self):
+        return {
+            "id": self.id,
+            "author": self.author,
+            "text": self.text,
+            "posted": str(self.posted)
+        }
+
 
 class CommentSchema(Schema):
-	id = fields.Integer()
-	author = fields.String()
-	text = fields.String()
-	posted = fields.DateTime()
+    id = fields.Integer()
+    author = fields.String()
+    text = fields.String()
+    posted = fields.DateTime()
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
+
 @app.route('/')
 @login_required
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/comments', methods=['GET', 'POST'])
 @login_required
 def comments():
-	if request.method == 'POST':
-		comment = Comment()
-		comment.author = request.form['author']
-		comment.text = request.form['text']
-		db.session.add(comment)
-		db.session.commit()
+    if request.method == 'POST':
+        comment = Comment()
+        comment.author = request.form['author']
+        comment.text = request.form['text']
+        db.session.add(comment)
+        db.session.commit()
 
-	comments = Comment.query.order_by(Comment.posted.desc())
-	serializer = CommentSchema(many=True)
-	result = serializer.dump(comments)
-	response = jsonify({"comments": result.data})
-	return response
+    comments = Comment.query.order_by(Comment.posted.desc())
+    serializer = CommentSchema(many=True)
+    result = serializer.dump(comments)
+    response = jsonify({"comments": result.data})
+    return response
+
 
 @manager.command
 def create_user(email, password):
-	user_datastore.create_user(email=email, password=password)
-	db.session.commit()
+    user_datastore.create_user(email=email, password=password)
+    db.session.commit()
+
 
 if __name__ == '__main__':
-	manager.run()
+    manager.run()
--- static/js/tutorial.js
+++ static/js/tutorial.js
@@ -1,8 +1,12 @@
-var converter = new Showdown.converter();
 
 var Comment = React.createClass({
+  rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
+
   render: function () {
-    var rawMarkup = converter.makeHtml(this.props.text.toString());
     var formattedDate = new Date(this.props.posted)
     return (
       <div className="comment">
@@ -10,7 +14,7 @@ var Comment = React.createClass({
           {this.props.author}
         </h2>
         <span>{formattedDate.toUTCString()}</span>
-        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
@@ -18,15 +22,14 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
   render: function () {
-    var commentNodes = [];
-    this.props.data.forEach(function (comment, index) {
-      commentNodes.push(
-        <Comment 
-          author={comment.author} 
-          text={comment.text} 
-          posted={comment.posted} 
-          key={index}>
-          {comment}
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment
+          author={comment.author}
+          text={comment.text}
+          posted={comment.posted}
+          key={comment.posted}>
+          {comment.text}
         </Comment>
       );
     });
@@ -38,35 +41,12 @@ var CommentList = React.createClass({
   }
 });
 
-var CommentForm = React.createClass({
-  handleSubmit: function (e) {
-    e.preventDefault();
-    var author = this.refs.author.getDOMNode().value.trim();
-    var text = this.refs.text.getDOMNode().value.trim();
-    if (!text || !author ) {
-      return;
-    }
-    this.props.onCommentSubmit({author: author, text: text, posted: new Date()})
-    this.refs.author.getDOMNode().value = '';
-    this.refs.text.getDOMNode().value = '';
-    return;
-  },
-  render: function () {
-    return (
-      <form className="commentForm" onSubmit={this.handleSubmit}>
-        <input type="textarea" placeholder="Your name" ref="author" />
-        <input type="text" placeholder="Say something..." ref="text" />
-        <input type="submit" value="Post" />
-      </form>
-    );
-  }
-});
-
 var CommentBox = React.createClass({
   loadCommentsFromServer: function () {
     return $.ajax({
       url: this.props.url,
       dataType: 'json',
+      cache: false,
       success: function (data) {
         this.setState({data: data.comments});
       }.bind(this),
@@ -77,20 +57,20 @@ var CommentBox = React.createClass({
   },
   handleCommentSubmit: function (comment) {
     var comments = this.state.data;
-    comments.push(comment);
-    this.setState({data: comments}, function () {
-      return $.ajax({
-        url: this.props.url,
-        dataType: 'json',
-        type: 'POST',
-        data: comment,
-        success: function (data) {
-          this.setState({data: data});
-        }.bind(this),
-        error: function (xhr, status, err) {
-          console.error(this.props.url, status, err.toString());
-        }.bind(this)
-      });
+    comment.id = Date.now();
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function (data) {
+        this.setState({data: data.comments});
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
     });
   },
   getInitialState: function () {
@@ -111,7 +91,49 @@ var CommentBox = React.createClass({
   }
 });
 
-React.render(
+var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function (e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author ) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text, posted: new Date()});
+    this.setState({author: '', text: ''});
+    return;
+  },
+  render: function () {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+ReactDOM.render(
   <CommentBox url="/comments" pollInterval={5000} />,
   document.getElementById('content')
 );
